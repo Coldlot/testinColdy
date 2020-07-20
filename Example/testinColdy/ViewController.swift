85e71ba05eb48c0f53924f2cb6aeb5fe0a9b89f4
Andrew Sergeev <sergeev.smartbox@gmail.com>
2020-07-21 01:08:42 +0300

    func push

--- Example/testinColdy/ViewController.swift
+++ Example/testinColdy/ViewController.swift
@@ -7,18 +7,15 @@
 //
 
 import UIKit
+import testinColdy
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pencilImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pencilImageView.applyBorder(color: .red, width: 4)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
